cd452ac39b9fffa3c44a87e5e5d53bf049b4c64b
arvind <arvind.tiwari@logicwares.com>
2015-06-11 17:30:51 +0530

    changes in all file

---
+++ app/models/product.rb
@@ -0,0 +1,34 @@
+class Product < ActiveRecord::Base
+
+    before_destroy :ensure_not_referenced_by_any_line_item
+	  validates :title, :description, presence: true
+	  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+    validates :image_url, allow_blank: true, format: {
+    with:
+        %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+      }
+
+     has_many :line_items
+     has_many :orders, through: :line_items
+
+
+
+
+    def self.latest
+     Product.order(:updated_at).last
+    end
+
+
+
+  private
+# ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+    return true
+    else
+      errors.add(:base, 'Line Items present')
+      return false
+    end
+  end
+end
